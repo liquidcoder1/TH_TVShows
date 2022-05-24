112dddf8a446fbd153b8c61521d4cfdbb8358e1c
johnkul001 <johnkul001@gmail.com>
2022-05-24 13:42:11 +0100

    Initial Commit

---
+++ DistilledMovieApp/ContentView.swift
@@ -0,0 +1,21 @@
+// File: ContentView.swift
+// Project: DistilledMovieApp
+
+// Created at 24/05/2022 by John
+// Visit https://www.liquidcoder.com to learn more
+// Copyright © Liquidcoder Ltd. All rights reserved
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
